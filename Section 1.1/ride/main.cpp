b829e8c625ab07be90ea010a6a73f3cd89d44927
Komron Bakozoda <komron.bakozoda@gmail.com>
2017-01-03 08:20:00 +0300

    [ride] add I/O files

--- Section 1.1/ride/main.cpp
+++ Section 1.1/ride/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+/*
+ ID: komron11
+ LANG: C++
+ PROG: ride
+ */
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    FILE* input;
+    FILE* output;
+
+    char groupName[10];
+    char ufo[10];
+
+    input = fopen("ride.in", "r");
+    output = fopen("ride.out", "w");
+
+    fscanf(input, "%s", groupName);
+    fscanf(input, "%s", ufo);
+
+    fprintf(output, "%s", ufo);
+
     return 0;
 }
