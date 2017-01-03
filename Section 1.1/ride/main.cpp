2deca0e5a6694bd3064b093497743d4b8c26be21
Komron Bakozoda <komron.bakozoda@gmail.com>
2017-01-03 08:33:46 +0300

    [ride] calculate products for each string, compare, print the decision

--- Section 1.1/ride/main.cpp
+++ Section 1.1/ride/main.cpp
@@ -12,8 +12,8 @@ int main()
     FILE* input;
     FILE* output;
 
-    char groupName[10];
-    char ufo[10];
+    char groupName[10] = {'\0'};
+    char ufo[10] = {'\0'};
 
     input = fopen("ride.in", "r");
     output = fopen("ride.out", "w");
@@ -21,7 +21,21 @@ int main()
     fscanf(input, "%s", groupName);
     fscanf(input, "%s", ufo);
 
-    fprintf(output, "%s", ufo);
+    int groupNameProduct = 1;
+    int ufoProduct = 1;
+
+    for (int i = 0; i < 6; i++) {
+        groupNameProduct *= (groupName[i] - 64);
+        ufoProduct *= (ufo[i] - 64);
+    }
+
+    groupNameProduct %= 47;
+    ufoProduct %= 47;
+
+    if (groupNameProduct == ufoProduct)
+        fprintf(output, "GO\n");
+    else
+        fprintf(output, "STAY\n");
 
     return 0;
 }
